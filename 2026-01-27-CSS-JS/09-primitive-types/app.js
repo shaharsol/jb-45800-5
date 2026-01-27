4cdc0da32b6eda23c564b25f4e92d343efad0444
shaharsol <shahar@gnift.com>
2026-01-27 15:57:44 +0200

    javascript started

---
+++ 2026-01-27-CSS-JS/09-primitive-types/app.js
@@ -0,0 +1,16 @@
+// 3 major types of values
+
+// string (text) - a collection of characters
+// string values will be represented as characters inside quotes (' " `)
+myName = "shahar"
+myAddress = "3 Homa Humigdal Tel Aviv 5005432"
+
+// numbers
+myAge = 50
+myKids = 3
+myBalance = -5000
+
+// boolean (yes/no truth/lie 0/1) like a switch that can be either on or off
+isMarried = true
+isDivorced = false
+
---
+++ 2026-01-27-CSS-JS/10-calculator/app.js
@@ -0,0 +1,14 @@
+// get input from user
+firstNumber = +prompt(`enter 1st number`); // assignment, takes the right value and put it in the left drawer
+secondNumber = +prompt('enter 2nd number');
+
+// process
+// result = Number(firstNumber) + Number(secondNumber)
+// result = +firstNumber + +secondNumber
+result = firstNumber + secondNumber
+// 7             2         + 5 
+
+// output
+// alert(firstNumber + secondNumber)
+alert(result)
+// 7 => "7"
