8c56ad0a837d31157f3134b601ff34417508bda5
shaharsol <shahar@gnift.com>
2026-01-29 13:43:40 +0200

    switch

---
+++ 2026-01-29-JS/06-class-exercise-1/app.js
@@ -0,0 +1,11 @@
+firstNumber = +prompt('enter first number')
+secondNumber = +prompt('enter second number')
+
+isFirstBigger = firstNumber > secondNumber
+
+if (isFirstBigger) { 
+    alert(`${firstNumber} is the bigger one`)
+} else {
+    alert(`${secondNumber} is the bigger one`)
+}
+
---
+++ 2026-01-29-JS/11-switch/app.js
@@ -0,0 +1,39 @@
+number = +prompt('enter number between 1 and 5')
+
+// if (number == 1) {
+//     alert ('one')
+// } else if (number == 2) {
+//     alert ('two')
+// } else if (number == 3) { 
+//     alert(`three`)
+// } else if (number == 4) { 
+//     alert(`four`)
+// } else if (number == 5) { 
+//     alert(`five`)
+// } else {
+//     alert(`unknown`)
+// }
+
+switch (number) {
+    case 1: 
+        alert('one')
+        break;
+    case 2: 
+        alert('two')
+        break;
+    case 3:
+        alert('three')
+        break;
+    case 4:
+        alert('four')
+        break;
+    case 5:
+        alert('five')
+        break;
+    default: 
+        alert('unknown')
+}
+
+
+
+
