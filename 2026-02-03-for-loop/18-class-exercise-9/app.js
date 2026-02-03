d66cde640a32be9f5f017df4671cb9cafe3efd05
shaharsol <shahar@gnift.com>
2026-02-03 14:06:15 +0200

    inner for

--- 2026-02-03-for-loop/18-class-exercise-9/app.js
+++ 2026-02-03-for-loop/18-class-exercise-9/app.js
@@ -1,6 +1,6 @@
 for(row = 1; row < 6; row = row + 1) {
-    column = '' // <- empty string
-    for(columns = 1; columns < row + 1; columns = columns + 1) {
+    column = ''
+    for(columns = 1; columns <= 2**row; columns = columns + 1) {
         column = column + '*'
     }
     console.log(column)
