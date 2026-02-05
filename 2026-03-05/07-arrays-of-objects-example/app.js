f35275b88ae7e7d708103a8c0e270644b32fc7df
shaharsol <shahar@gnift.com>
2026-02-05 11:19:26 +0200

    arrays of objects

---
+++ 2026-03-05/07-arrays-of-objects-example/app.js
@@ -0,0 +1,16 @@
+users = [
+    {
+        id: '22',
+        name: 'Moshe',
+        following: ['24']
+    }, {
+        id: '23',
+        name: 'Eilat',
+        following: []
+    }, {
+        id: '24',
+        name: 'Golan',
+        following: ['22', '23']
+    }
+]
+
