2bcabdb8a6c917f864d4a6f031cb9793bf44ce83
shaharsol <shahar@gnift.com>
2026-02-10 15:53:21 +0200

    DHTML

---
+++ 2026-02-10/13-dom/app.js
@@ -0,0 +1,5 @@
+function colorAmet() {
+    const span = document.body.children[0].children[1].children[0].children[2].children[0]
+    // span.style = "color: red;"
+    span.className = 'red'
+}
---
+++ 2026-02-10/14-get-element-by-id/app.js
@@ -0,0 +1,19 @@
+function colorAmet() {
+    // we don't want to navigate DOM this way... it's too hard... tedious
+    // const span = document.body.children[0].children[1].children[0].children[2].children[0]
+
+    // this is the BEST approach to obtaining the right html element to manipulate
+    const span = document.getElementById('amet')
+
+    // span.style = "color: red;"
+    span.className = 'red'
+
+    // however, i can access elements in the dom using CSS selectors
+    // use it if you dont have access to add id's to the HTML
+    const otherSpan = document.querySelector('p > span')
+    otherSpan.className = 'red'
+    
+    const allTheParagraphs = document.querySelectorAll('p')
+    console.log(allTheParagraphs)
+
+}
