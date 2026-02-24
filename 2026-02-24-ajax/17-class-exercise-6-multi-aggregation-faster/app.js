e8ddf28f84ba44d946e546bfeb7d8f71329bfc6e
shaharsol <shahar@gnift.com>
2026-02-24 14:50:06 +0200

    super reduction

--- 2026-02-24-ajax/17-class-exercise-6-multi-aggregation-faster/app.js
+++ 2026-02-24-ajax/17-class-exercise-6-multi-aggregation-faster/app.js
@@ -13,8 +13,25 @@ const loadList = async () => {
             </tr>`)
             .join('')
 
-        document.getElementById('averageHeight').innerHTML = 
-            users.reduce((sumHeight, { height }) => sumHeight + height, 0) / users.length
+        const reduction = 
+            users.reduce((cumulative, { height, gender }) => {
+                cumulative[gender].sum += height
+                cumulative[gender].count += 1
+                return cumulative
+            }, {
+                male: {
+                    sum: 0,
+                    count: 0
+                },
+                female: {
+                    sum: 0,
+                    count: 0
+                }
+            })
+
+        // console.log(reduction)
+        document.getElementById('averageMaleHeight').innerHTML = reduction.male.sum / reduction.male.count
+        document.getElementById('averageFemaleHeight').innerHTML = reduction.female.sum / reduction.female.count
 
     } catch (e) {
         console.log(e)
