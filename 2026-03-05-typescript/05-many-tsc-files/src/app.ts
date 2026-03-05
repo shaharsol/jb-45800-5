fc0e94951373cb6893d2b21dae7bf759f868a410
shaharsol <shahar@gnift.com>
2026-03-05 13:48:18 +0200

    alota

--- 2026-03-05-typescript/05-many-tsc-files/src/app.ts
+++ 2026-03-05-typescript/05-many-tsc-files/src/app.ts
@@ -3,6 +3,7 @@ const anotherMessage = 'hello zohar and Omri and Liran'
 console.log(message)
 console.log(anotherMessage);
 
+// added a comment
 (async() => {
     const response = await fetch('https://google.com')
 })()
---
+++ 2026-03-05-typescript/07-types/src/app.ts
@@ -0,0 +1,63 @@
+const message: string = 'hello world'
+const anotherMessage: string = ['true']
+
+// which types do we have?
+
+// all the known primitive types
+const someNumber: number = 98
+const someString: string = 'some string'
+const someBoolean: boolean = false
+const anotherBoolean = 5 > 4
+
+// arrays of primitives
+const grades: number[] = [33, 55, 56]
+const names: string[] = ['fsdfsd', 'fdgfdgdf']
+const someFlags: boolean[] = [true, false, true]
+
+// objects
+const student: object = {}
+let anotherStudent: object = {
+    id: 1,
+    name: 'moshe'
+}
+anotherStudent = {
+    identificaton: 8,
+    familyName: 'Katz'
+}
+
+// composite Type
+type Student = {
+    id: number,
+    name: string,
+    address: {
+        street: string,
+        streetNumber: number
+    }
+}
+
+let someStudent: Student = {
+    id: 1,
+    name: 'moshe',
+    address: {
+        street: 'Herzl',
+        streetNumber: 8
+    }
+}
+
+let anotherSomeStudent: Student = {
+    id: 4,
+    name: "Hagit",
+    address: {
+        street: "Golan",
+        streetNumber: 98
+    }
+}
+
+const students: Student[] = [someStudent, anotherSomeStudent, {
+    id: 0,
+    name: "",
+    address: {
+        street: "",
+        streetNumber: 0
+    }
+}]
