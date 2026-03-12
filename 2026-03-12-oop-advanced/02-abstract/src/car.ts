813b2182ca14186728b26f4885e94bb1a2dc2dd9
shaharsol <shahar@gnift.com>
2026-03-12 10:52:53 +0200

    abstarct

---
+++ 2026-03-12-oop-advanced/02-abstract/src/car.ts
@@ -0,0 +1,20 @@
+import Vehicle from './vehicle.js'
+
+export default class Car extends Vehicle{
+    numberOfDoors: number
+    static readonly numberOfWheels: number = 4
+
+    steerWheel() {
+        console.log('steering wheel...')
+    }
+
+    constructor (make: string, engineVolume: number, horsePower: number, color: string, model: number, priceBeforeTax: number, numberOfDoors: number) {
+        super(make, engineVolume, horsePower, color, model, priceBeforeTax) // super is a special way to invoke the ancestor constructor
+        this.numberOfDoors = numberOfDoors
+        // i can access protected members from within the inheritence hierarchy
+        console.log(`this car custom clerk is ${this.customClerk}`)
+        // but i i cannot access private members even from within the inheritence hierarchy
+        // console.log(`this car bakshish beneficior is ${this.bakshishBeneficior}`)
+    }
+
+}
