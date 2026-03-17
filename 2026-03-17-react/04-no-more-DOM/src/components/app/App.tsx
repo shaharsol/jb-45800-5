77a8fd82a7ea11391561534d1073e047b4f6d119
shaharsol <shahar@gnift.com>
2026-03-17 15:45:05 +0200

    no more DOM

---
+++ 2026-03-17-react/04-no-more-DOM/src/components/app/App.tsx
@@ -0,0 +1,15 @@
+// 2. you must import the css so it is included in the bundle
+import './App.css'
+
+function App() {
+
+  return (
+    // 3. you must add a class to the root elenment of the component
+    <div className="App">
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim, blanditiis!</p>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim, blanditiis!</p>
+    </div>
+  )
+}
+
+export default App
