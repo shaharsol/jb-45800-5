86c758146d146c1d8f3303559820299538e4543a
shaharsol <shahar@gnift.com>
2026-03-17 15:53:02 +0200

    returning jsx

---
+++ 2026-03-17-react/05-returning-jsx/src/components/app/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (  
+    <div className='App'>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, iste.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, iste.</p>
+    </div>
+  )
+}
+
+export default App
