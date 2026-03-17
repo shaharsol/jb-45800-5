208b2294ca41de6a6ea39c2f10d1f3f7eb0aac6f
shaharsol <shahar@gnift.com>
2026-03-17 15:20:57 +0200

    css scoping done

--- 2026-03-17-react/03-css-scoping/src/components/app/App.tsx
+++ 2026-03-17-react/03-css-scoping/src/components/app/App.tsx
@@ -1,9 +1,14 @@
+// 2. you must import the css so it is included in the bundle
 import './App.css'
 
 function App() {
 
   return (
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim, blanditiis!</p>
+    // 3. you must add a class to the root elenment of the component
+    <div className="App">
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim, blanditiis!</p>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim, blanditiis!</p>
+    </div>
   )
 }
 
--- 2026-03-17-react/03-css-scoping/src/main.tsx
+++ 2026-03-17-react/03-css-scoping/src/main.tsx
@@ -5,6 +5,8 @@ import App from './components/app/App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <h1>hello this is my CSS scoping example</h1>
+    <p>i am a green paragraph</p>
     <App />
   </StrictMode>,
 )
