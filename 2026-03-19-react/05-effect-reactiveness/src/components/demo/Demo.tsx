da8ea7af7631df2f1b07b9661c2c2a9d453e8d12
shaharsol <shahar@gnift.com>
2026-03-19 15:00:28 +0200

    reactiveness

---
+++ 2026-03-19-react/05-effect-reactiveness/src/components/demo/Demo.tsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from 'react'
+import './Demo.css'
+
+// these functions emulate data that originates from OUTSIDE of the component
+// form a database, a remote server, a file, whatever
+// these functions are not part of the component
+function getDogs(): string[] {
+    return ['Kate', 'Dubi', 'Betty']
+}
+
+function getCats(): string[] {
+    return ['Meow', 'Mitzi', 'Sophie']
+}
+
+
+export default function Demo() {
+    const [animals, setAnimals] = useState<string[]>([])
+    const [isDogs, setIsDogs] = useState<boolean>(true)
+
+    function toggle() {
+        setIsDogs(!isDogs)
+    }
+
+    useEffect(() => {
+        setAnimals(isDogs ? getDogs() : getCats())
+    }, [isDogs])
+
+    return (
+        <div className="Demo">
+            <select onChange={toggle}>
+                <option value="dogs">Dogs</option>
+                <option value="cats">Cats</option>
+            </select>
+            <ul>
+                {animals.map(animal => <li>{animal}</li>)}
+            </ul>
+        </div>
+    )
+}
