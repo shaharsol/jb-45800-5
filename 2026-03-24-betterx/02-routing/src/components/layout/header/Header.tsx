76c291372b569f1c2074b752c2c7199ae1a3b907
shaharsol <shahar@gnift.com>
2026-03-24 12:01:27 +0200

    routing done

---
+++ 2026-03-24-betterx/02-routing/src/components/layout/header/Header.tsx
@@ -0,0 +1,22 @@
+import { Link, NavLink } from 'react-router-dom'
+import './Header.css'
+
+export default function Header() {
+  return (
+    <div className='Header'>
+      <div>
+        Logo
+      </div>
+      <div>
+        {/* NEVER use <a> tags to navigate inside an SPA app */}
+        {/* <a href="/profile">Profile</a> | <a href="/feed">Feed</a> */}
+
+        {/* <Link to="/profile">Profile</Link> | <Link to="/feed">Feed</Link> */}
+        <NavLink to="/profile">Profile</NavLink> | <NavLink to="/feed">Feed</NavLink>
+      </div>
+      <div>
+        Welcome Alice | Logout
+      </div>
+    </div>
+  )
+}
