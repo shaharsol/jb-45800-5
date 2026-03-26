f0cbdccfceab3a4f814a45ea3a5c7b8f8e4cd54a
shaharsol <shahar@gnift.com>
2026-03-26 14:13:17 +0200

    general hooks

--- 2026-03-26-crud/03-create/src/components/posts/new/NewPost.tsx
+++ 2026-03-26-crud/03-create/src/components/posts/new/NewPost.tsx
@@ -12,19 +12,50 @@ export default function NewPost(props: NewPostProps) {
     const { addPost } = props
 
     async function createPost(draft: PostDraft) {
-        const newPost = await profileService.createPost(draft)
-        // change state, show the new post in the profile...
-        reset()
-        addPost(newPost)
+        try {
+            const newPost = await profileService.createPost(draft)
+            // change state, show the new post in the profile...
+            reset()
+            addPost(newPost)
+        } catch (e) {
+            alert (e)
+        }
     }
 
-    const { handleSubmit, register, reset } = useForm<PostDraft>()
+    // an example of an attempt to use hook *not inside* the root of the component function
+    // rather in a {} block. this will be blocked by eslint
+    // if(true) {
+    //     const {formState} = useForm<PostDraft>()
+    // }
+
+    const { handleSubmit, register, reset, formState } = useForm<PostDraft>()
 
     return (
         <div className='NewPost'>
             <form onSubmit={handleSubmit(createPost)}>
-                <input placeholder='title' {...register('title')} />
-                <textarea placeholder='body' {...register('body')}></textarea>
+                <input placeholder='title' {...register('title', {
+                    required: {
+                        value: true,
+                        message: 'title is a required field'
+                    },
+                    minLength: {
+                        value: 10,
+                        message: 'title must be at least 10 characters'
+                    }
+
+                })} />
+                <div className='error'>{formState.errors.title?.message}</div>
+                <textarea placeholder='body' {...register('body', {
+                    required: {
+                        value: true,
+                        message: 'body is a required field'
+                    },
+                    minLength: {
+                        value: 20,
+                        message: 'body must be at least 20 characters'
+                    }
+                })}></textarea>
+                <div className='error'>{formState.errors.body?.message}</div>
                 <button>Add Post</button>
             </form>
         </div>
--- 2026-03-26-crud/03-create/src/utils/dates.ts
+++ 2026-03-26-crud/03-create/src/utils/dates.ts
@@ -1,3 +1,8 @@
+// import { useState } from "react"
+
 export function displayDate(date: string): string {
+
+    // an example of an attempt to use an hook not from a component or from another hook
+    // const [something, setSomehting ] = useState<string>('')
     return (new Date(date)).toLocaleDateString()
 }
