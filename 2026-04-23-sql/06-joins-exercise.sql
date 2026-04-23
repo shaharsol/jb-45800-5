7a9295b438fb424e9c10dabab20f4a89cf43efd1
shaharsol <shahar@gnift.com>
2026-04-23 12:08:35 +0300

    finsihed select

--- 2026-04-23-sql/06-joins-exercise.sql
+++ 2026-04-23-sql/06-joins-exercise.sql
@@ -26,3 +26,16 @@ join products on products.id = order_details.productId
 join suppliers on suppliers.id = products.supplierId
 group by suppliers.id
 order by sum_quantity desc;
+
+-- Calculate the imports sum (in $) from each country to the USA
+select supplier_country.name, sum(products.price * order_details.quantity) as sum_per_country
+from orders 
+join countries 
+on orders.countryId = countries.id 
+and countries.name = 'USA'
+join `order-details` as order_details on order_details.orderId = orders.id
+join products on products.id = order_details.productId
+join suppliers on suppliers.id = products.supplierId
+join countries as supplier_country on supplier_country.id = suppliers.countryId
+group by supplier_country.id
+order by sum_per_country desc;
