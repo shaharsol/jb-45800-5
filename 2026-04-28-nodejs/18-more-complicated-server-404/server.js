d099ef4aeaffc3dfc4a94954f62a947f618e359d
shaharsol <shahar@gnift.com>
2026-04-28 14:29:57 +0300

    nodemon

---
+++ 2026-04-28-nodejs/18-more-complicated-server-404/server.js
@@ -0,0 +1,35 @@
+// import createServer function from the built in http package
+const { createServer } = require('http')
+
+// realize port
+const port = process.env.PORT || 3000
+
+// create a request handler
+const requestHandler = (request, response) => {
+    switch(request.url) {
+        case '/health':
+            response.end('healthy')
+            break;
+        case '/hello':
+            response.end('hello')
+            break;
+        case '/users':
+            response.end('users list....')
+            break;
+        default:
+            response.writeHead(404)
+            response.end('page not found 404')
+            break;
+    }
+}
+
+// create a server object
+const server = createServer(requestHandler)
+
+// start the server by listening on a
+server.listen(port);
+
+// notice: 
+// when i program a server in Node.js
+// all i do is to actually implement
+// a single function: the request handler
