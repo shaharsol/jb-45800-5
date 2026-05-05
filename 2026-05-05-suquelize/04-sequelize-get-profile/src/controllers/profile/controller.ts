710a7690a64f9646fe153c2ad90680abc9c7f9ad
shaharsol <shahar@gnift.com>
2026-05-05 15:06:47 +0300

    get single post

--- 2026-05-05-suquelize/04-sequelize-get-profile/src/controllers/profile/controller.ts
+++ 2026-05-05-suquelize/04-sequelize-get-profile/src/controllers/profile/controller.ts
@@ -12,9 +12,31 @@ export async function getProfile(request: Request, response: Response, next: Nex
 
     // since we have sequelize, and User already has a posts: Post[]
     // declaration in the HasMany, we can simply fetch the user
-    const user = await User.findByPk(userId, {
-        include: [{
-            model: Post,
+    try {
+        const user = await User.findByPk(userId, {
+            include: [{
+                model: Post,
+                include: [
+                    User,
+                    { 
+                        model: Comment,
+                        include: [ User ]
+                    }
+                ]
+            }]
+        })
+        response.json(user.posts)
+
+    } catch (e) {
+        next(e)
+    }
+}
+
+export async function getPost(request: Request<{postId: string}>, response: Response, next: NextFunction) {
+    try {
+        const { postId } = request.params
+
+        const post = await Post.findByPk(postId, {
             include: [
                 User,
                 { 
@@ -22,12 +44,18 @@ export async function getProfile(request: Request, response: Response, next: Nex
                     include: [ User ]
                 }
             ]
-        }]
-    })
+        })
 
+        if(!post) return next({
+            status: 404,
+            message: 'post does not exist'
+        })
 
-    response.json(user)
+        response.json(post)
 
+    } catch (e) {
+        next(e)
+    }
 }
 
 
