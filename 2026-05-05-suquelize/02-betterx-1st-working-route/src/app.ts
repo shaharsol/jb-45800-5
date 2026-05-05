b6232ac3265797c1f4632f7831617ed9cbd60c7e
shaharsol <shahar@gnift.com>
2026-05-05 09:52:26 +0300

    feed router

--- 2026-05-05-suquelize/02-betterx-1st-working-route/src/app.ts
+++ 2026-05-05-suquelize/02-betterx-1st-working-route/src/app.ts
@@ -4,6 +4,7 @@ import config from 'config'
 import respondError from './middlewares/error/error-responder'
 import notFound from './middlewares/not-found'
 import profileRouter from './routers/profile'
+import feedRouter from './routers/feed'
 
 const port = config.get<number>('app.port')
 const name = config.get<string>('app.name')
@@ -13,6 +14,7 @@ const app = express()
 
 // middlewares
 app.use('/profile', profileRouter)
+app.use('/feed', feedRouter)
 app.use('/', notFound)
 
 // error middlewares
