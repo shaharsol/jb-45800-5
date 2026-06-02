2295283b4e54ceb910026152d2bf9a7f9366a107
shaharsol <shahar@gnift.com>
2026-06-02 13:48:57 +0300

    model and service for generate pic in front

--- 2026-06-02-llm/01-betterx-llm-features/frontend/src/services/auth-aware/DraftsService.ts
+++ 2026-06-02-llm/01-betterx-llm-features/frontend/src/services/auth-aware/DraftsService.ts
@@ -1,9 +1,15 @@
 import AuthAwareService from "./AuthAware";
 import type DraftImproveResponse from "../../models/DraftImproveResponse";
+import type DraftGeneratePicResponse from "../../models/DraftGeneratePicResponse";
 
 export default class DraftsService extends AuthAwareService {
     async improve(body: string): Promise<DraftImproveResponse> {
         const { data } = await this.axiosInstance.post<DraftImproveResponse>(`/drafts/improve`, { body })
+        return data
+    }
+
+    async generatePic(title: string, body: string): Promise<DraftGeneratePicResponse> {
+        const { data } = await this.axiosInstance.post<DraftGeneratePicResponse>(`/drafts/pic`, { title, body })
         return data
     }
 }
