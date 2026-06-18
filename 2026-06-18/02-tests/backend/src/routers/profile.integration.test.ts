54086c46dc313c2eb78d9e47ef042e9e75abcc19
shaharsol <assaf.fink@gmail.com>
2026-06-18 10:36:42 +0300

    almost finalized integration tests

--- 2026-06-18/02-tests/backend/src/routers/profile.integration.test.ts
+++ 2026-06-18/02-tests/backend/src/routers/profile.integration.test.ts
@@ -18,6 +18,9 @@ import app, { init } from "../app"
 import request from 'supertest'
 import sequelize from '../db/sequelize'
 import pgvector from '../db/pgvector'
+import { randomUUID } from "crypto"
+import { sign } from "jsonwebtoken"
+import config from 'config'
 
 afterAll(async () => {
     await sequelize.close()
@@ -33,6 +36,18 @@ describe('profile router integration tests', () => {
             const response = await request(app).get('/profile')
             // run expectations on the response
             expect(response.statusCode).toBe(401)
+            expect(response.text).toBe('auth header is missing!')
+        })
+        it('returns an array of posts when auth header is present', async() => {
+            const userId = '1230ae30-dc4f-4752-bd84-092956f5c633'
+            const jwt = sign({id: userId}, config.get<string>('app.encryptionKey'))
+            await init()
+            const response = await request(app).get('/profile').set('Authorization', `Bearer ${jwt}`)
+            expect(response.statusCode).toBe(200)
+            expect(response.body).toBeInstanceOf(Array)
+            expect(response.body.length).toBe(6)
+            expect(response.body[0].title).toBe("Need 2-player board game recommendations")
+
         })
     })
 })
