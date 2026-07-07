932e439cbd9e72fe1e4d61a24d8d26b443b5e40f
shaharsol <assaf.fink@gmail.com>
2026-07-07 12:01:37 +0300

    mapa2

--- 2026-07-07-recursion-order-of-magnitude/12-number-of-islands/demo.js
+++ 2026-07-07-recursion-order-of-magnitude/12-number-of-islands/demo.js
@@ -1,7 +1,7 @@
 const mapa = [
     ['1','1','1','1','1','1','1','0','0'],
     ['1','1','1','1','0','0','0','0','0'],
-    ['1','1','1','0','0','0','0','0','0'],
+    ['1','1','1','0','0','0','0','0','1'],
     ['1','1','0','1','0','0','0','0','0'],
     ['1','1','0','1','0','0','0','0','0'],
     ['1','1','0','1','1','0','1','1','0'],
@@ -10,4 +10,9 @@ const mapa = [
     ['0','0','0','0','0','0','0','0','0'],
 ]
 
-// in the mapa, there are islands
+// in the mapa, there are islands and oceans
+// '1' represent land
+// '0' represent water
+// an island is considered a group of '1' that are connected
+// in straight lines (not diagonal)
+// write a program to echo the number of islands in the mapa
