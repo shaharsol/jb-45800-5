61d7615a9bdc501f1c18f8c7c6770221a124d8f1
shaharsol <assaf.fink@gmail.com>
2026-07-16 10:56:08 +0300

    avg and sum

--- 2026-07-16/01-mongo-aggregations/excercise1.js
+++ 2026-07-16/01-mongo-aggregations/excercise1.js
@@ -1,2 +1,20 @@
 // calculate the top 5 cities, per total shipping fee per city
 // 
+db.orders.aggregate([
+    {
+        $group: {
+            _id: "$ship_city",
+            totalShippingCost: {$sum: "$shipping_fee"},
+            numberOfOrders: {$sum: 1},
+            averageShippingCost: {$avg: "$shipping_fee"}
+        }
+    },
+    {
+        $sort: {
+            totalShippingCost: -1
+        }
+    },
+    {
+        $limit: 5
+    }
+])
