78d85cf0533f9ea55de18f69a1266fa10da583ff
shaharsol <shahar@gnift.com>
2026-07-21 10:39:16 +0300

    first cheerio work

--- 2026-07-21-scraping/01-ivory-scraper/src/scrape-product.ts
+++ 2026-07-21-scraping/01-ivory-scraper/src/scrape-product.ts
@@ -1,13 +1,28 @@
 import axios = require("axios")
+import * as cheerio from 'cheerio';
 
 async function scrapeProduct() {
     try {
         const { data } = await axios.get('https://www.ivory.co.il/catalog.php?id=113259')
-        const barcodeSpecificAreaIndex = data.lastIndexOf('barcode-specific-area');
-        const nextTagCloserIndex = data.indexOf('>', barcodeSpecificAreaIndex)
-        const nextTagOpenerIndex = data.indexOf('<', nextTagCloserIndex)
-        const result = data.substring(nextTagCloserIndex + 1, nextTagOpenerIndex)
+        // const barcodeSpecificAreaIndex = data.lastIndexOf('barcode-specific-area');
+        // const nextTagCloserIndex = data.indexOf('>', barcodeSpecificAreaIndex)
+        // const nextTagOpenerIndex = data.indexOf('<', nextTagCloserIndex)
+        // const result = data.substring(nextTagCloserIndex + 1, nextTagOpenerIndex)
+        const $ = cheerio.load(data);
+        const result = $('.barcode-specific-area').html()
+
+
         console.log(result)
+
+
+        // we don't want to scrape using string functions because
+        // 1. it is bad performance to search strings, better traverse a DOM object
+        // 2. it creates unreadable code
+        // const h1Index = data.indexOf('<h1')
+        // const h1Closer = data.indexOf('>', h1Index)
+        // const nextOpener = data.indexOf('<', h1Closer)
+        // const productTitle = data.substring(h1Closer + 1, nextOpener)
+        // console.log(productTitle)
     } catch (e) {
         console.error(e)
     }
