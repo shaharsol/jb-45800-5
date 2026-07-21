62b24a0d819b0da205999b867d0f5eca2fa061fc
shaharsol <shahar@gnift.com>
2026-07-21 10:05:21 +0300

    extarcting barcode

--- 2026-07-21-scraping/01-ivory-scraper/src/scrape-product.ts
+++ 2026-07-21-scraping/01-ivory-scraper/src/scrape-product.ts
@@ -4,7 +4,10 @@ async function scrapeProduct() {
     try {
         const { data } = await axios.get('https://www.ivory.co.il/catalog.php?id=113259')
         const barcodeSpecificAreaIndex = data.lastIndexOf('barcode-specific-area');
-        console.log(barcodeSpecificAreaIndex)
+        const nextTagCloserIndex = data.indexOf('>', barcodeSpecificAreaIndex)
+        const nextTagOpenerIndex = data.indexOf('<', nextTagCloserIndex)
+        const result = data.substring(nextTagCloserIndex + 1, nextTagOpenerIndex)
+        console.log(result)
     } catch (e) {
         console.error(e)
     }
