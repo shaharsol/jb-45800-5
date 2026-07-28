f787bf1d803353490f1d812582ef96f5589f9479
shaharsol <shahar@gnift.com>
2026-07-28 09:59:28 +0300

    static types

---
+++ 2026-07-28-python/02-types/app.py
@@ -0,0 +1,15 @@
+name: str = "shahar"
+num: int = 67
+flo: float = 67.7
+is_adult: bool = True
+is_adult = False
+some_value = None # snail_case and not camelCase
+print(name)
+print(num)
+print(flo)
+print(is_adult)
+print(some_value)
+
+# i can assign any value to any variable regardless of any static type declaration
+name = 3
+print(name)
