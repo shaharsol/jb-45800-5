e0a082ec152833b7c4f0359a03ef433ad2c87897
shaharsol <shahar@gnift.com>
2026-07-28 10:20:19 +0300

    inputs

---
+++ 2026-07-28-python/04-input/app.py
@@ -0,0 +1,5 @@
+first_name: str = input("enter your first name: ")
+last_name: str = input("enter your last name: ")
+age: int = int(input("enter your age: "))
+
+print(f"your name is {first_name} {last_name} and your age is {age}")
