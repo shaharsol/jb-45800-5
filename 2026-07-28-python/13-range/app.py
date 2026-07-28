6a0f5960ff866afc9ab83a975dee621de51eaea6
shaharsol <shahar@gnift.com>
2026-07-28 13:21:04 +0300

    ranges

---
+++ 2026-07-28-python/13-range/app.py
@@ -0,0 +1,22 @@
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9 ,10]
+
+# for number in numbers:
+#     print(number)
+
+# prints 0-9, i.e. the list of offsets of a 10 items array
+# for number in range(10):
+#     print(number)
+
+for number in range(1, 11):
+    print(number)    
+
+print("----------------------")
+
+for number in range(1, 11, 2):
+    print(number)    
+
+print("----------------------")
+
+for number in range(20, 10, -1):
+    print(number)    
+
---
+++ 2026-07-28-python/14-class-exercise-4/app.py
@@ -0,0 +1,5 @@
+num1: int = int(input("enter start number: "))
+num2: int = int(input("enter finish number: "))
+
+for number in range(num1, num2 + 1):
+    print(number)
