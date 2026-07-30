4456dd1210c7e06bcf894843b9759b43aec88e58
shaharsol <shahar@gnift.com>
2026-07-30 11:48:36 +0300

    mul func initial

--- 2026-07-30-python/13-lambdas-return-value-exercise/app.py
+++ 2026-07-30-python/13-lambdas-return-value-exercise/app.py
@@ -1,16 +1,4 @@
-# def say_hello():
-#     print("hello")
+def invoke_sum_function(mul_func: function):
+    return mul_func(10, 20)
 
-def invoke_another_function(func: function):
-    func()
-
-# def get_sum(a: int, b: int):
-#     return a + b
-
-def invoke_sum_function(sum_func: function):
-    return sum_func(10, 20)
-
-# this is an inline anonymous "arrow" function
-# similar to (x, y) => x + y in js
-invoke_another_function(lambda: print("hello from a lambda function"))   
 print(f"10 + 20 = {invoke_sum_function(lambda a, b: a + b)}")
