1ba4d1920627175e55377852281c0b4ef4fa7e6f
shaharsol <shahar@gnift.com>
2026-07-30 11:47:30 +0300

    13

---
+++ 2026-07-30-python/13-lambdas-return-value-exercise/app.py
@@ -0,0 +1,16 @@
+# def say_hello():
+#     print("hello")
+
+def invoke_another_function(func: function):
+    func()
+
+# def get_sum(a: int, b: int):
+#     return a + b
+
+def invoke_sum_function(sum_func: function):
+    return sum_func(10, 20)
+
+# this is an inline anonymous "arrow" function
+# similar to (x, y) => x + y in js
+invoke_another_function(lambda: print("hello from a lambda function"))   
+print(f"10 + 20 = {invoke_sum_function(lambda a, b: a + b)}")
