0a0d7ee587a2667dd26a73524608a7e6b1881eb1
shaharsol <shahar@gnift.com>
2026-07-30 11:54:03 +0300

    max ternary

---
+++ 2026-07-30-python/14-ternary-lambda/app.py
@@ -0,0 +1,9 @@
+def print_max(max_function: function):
+    number1 = int(input("enter a number: "))
+    number2 = int(input("enter a number: "))
+
+    max: int = max_function(number1, number2)
+
+    print(f"max number is {max}")
+
+    
