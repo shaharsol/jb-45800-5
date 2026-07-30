121a8765f38078868c7e4c70bed65ef15eb5105d
shaharsol <shahar@gnift.com>
2026-07-30 11:50:59 +0300

    mul

--- 2026-07-30-python/13-lambdas-return-value-exercise/app.py
+++ 2026-07-30-python/13-lambdas-return-value-exercise/app.py
@@ -1,4 +1,6 @@
-def invoke_sum_function(mul_func: function):
-    return mul_func(10, 20)
+def invoke_sum_function(mul_func: function, a: int, b: int):
+    return mul_func(a, b)
 
-print(f"10 + 20 = {invoke_sum_function(lambda a, b: a + b)}")
+number1 = int(input("enter a number "))
+number2 = int(input("enter another number "))
+print(f"{number1} * {number2} = {invoke_sum_function(lambda a, b: a * b, number1, number2)}")
