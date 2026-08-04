d867243db168bfe0cd69a939690be88bc7e03b06
shaharsol <shahar@gnift.com>
2026-08-04 09:59:44 +0300

    inheritence start

---
+++ 2026-08-04-python/03-inheritence/app.py
@@ -0,0 +1,5 @@
+from employee import Employee
+
+
+employee = Employee('yosi','shemesh', 20000)
+print(employee)
---
+++ 2026-08-04-python/03-inheritence/employee.py
@@ -0,0 +1,9 @@
+from person import Person
+
+class Employee(Person):
+    def __init__(self, first_name: str, last_name: str, salary: float):
+        super().__init__(first_name, last_name)
+        self.salary = salary
+
+    def __str__(self):
+        return f"my name is {self.first_name} and i earn {self.salary}"
