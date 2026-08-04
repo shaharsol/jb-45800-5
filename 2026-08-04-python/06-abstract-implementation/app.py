f89bfff456283ce6e5d1a9a0166a03982467e49f
shaharsol <shahar@gnift.com>
2026-08-04 11:09:50 +0300

    abstract contd

---
+++ 2026-08-04-python/06-abstract-implementation/app.py
@@ -0,0 +1,13 @@
+
+from circle import Circle
+
+
+circle = Circle(10, 20, 40)
+circle.display()
+
+
+if circle.x == 10:
+    # print('x is 10')
+    pass # pass enables me to maintain the sytnax without actually having code
+else:
+    print('x is not 10')
